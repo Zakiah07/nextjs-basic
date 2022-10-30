d32da80a43dcc00129bc65fc20ff622e85a7fea6
Zakiah07 <zakiah_bloom@yahoo.com>
2022-10-30 13:57:05 +0800

    coin API updated

--- pages/coins/index.js
+++ pages/coins/index.js
@@ -1,3 +1,34 @@
-const CoinList = () => {};
+import Axios from "axios";
+
+const CoinList = ({ coinData }) => {
+  console.log(coinData);
+  return (
+    <div>
+      {coinData.coins.map((coin) => {
+        return (
+          <div>
+            <h1> {coin.name} </h1>
+            <img src={coin.icon} />
+            <p> {coin.price} </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export const getStaticProps = async () => {
+  //   const id = params.id;
+  const data = await Axios.get(
+    "https://api.coinstats.app/public/v1/coins?skip=0"
+  );
+
+  return {
+    props: {
+      coinData: data.data,
+    },
+    revalidate: 10,
+  };
+};
 
 export default CoinList;
